f4d3227dc632185728bc39132a42c7efaa8e6309
Rafael <rafaelsouzavalle@gmail.com>
2022-12-16 14:25:54 -0300

    Recrie projeto para nova versão (Java 17)
    Ação para solução de conflito entre a versão do Java e a versão mais atual do Spring Boot. Nesse, caso, como o projeto é compilado pelo assistente, foi necessário recria-lo, adequando a versão do Java, para então inserir nele as classes já criadas.

---
+++ RequisicaoDemo/src/main/java/br/com/digivalle/RequisicaoDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.digivalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequisicaoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequisicaoDemoApplication.class, args);
+	}
+
+}
